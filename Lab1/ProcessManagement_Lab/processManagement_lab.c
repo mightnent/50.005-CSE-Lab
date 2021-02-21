af3f4481a5edf42a730fbe3547dd32433a8b4885
nugglet <victoriayong@gmail.com>
2021-02-21 17:05:34 +0800

    task 4b

--- Lab1/ProcessManagement_Lab/processManagement_lab.c
+++ Lab1/ProcessManagement_Lab/processManagement_lab.c
@@ -261,113 +261,6 @@ void cleanup(){
     // 1. Detach both shared memory (global_data and jobs)
     // 2. Delete both shared memory (global_data and jobs)
     // 3. Unlink all semaphores in sem_jobs_buffer
-}
-
-// Real main
-// int main(int argc, char* argv[]){
-
-//     printf("Lab 1 Starts...\n");
-
-//     struct timeval start, end;
-//     long secs_used,micros_used;
-
-//     //start timer
-//     gettimeofday(&start, NULL);
-
-//     //Check and parse command line options to be in the right format
-//     if (argc < 2) {
-//         printf("Usage: sum <infile> <numprocs>\n");
-//         exit(EXIT_FAILURE);
-//     }
-
-
-//     //Limit number_of_processes into 10. 
-//     //If there's no third argument, set the default number_of_processes into 1.  
-//     if (argc < 3){
-//         number_of_processes = 1;
-//     }
-//     else{
-//         if (atoi(argv[2]) < MAX_PROCESS) number_of_processes = atoi(argv[2]);
-//         else number_of_processes = MAX_PROCESS;
-//     }
-
-//     setup();
-//     createchildren();
-//     main_loop(argv[1]);
-
-//     //parent cleanup
-//     cleanup();
-
-//     //stop timer
-//     gettimeofday(&end, NULL);
-
-//     double start_usec = (double) start.tv_sec * 1000000 + (double) start.tv_usec;
-//     double end_usec =  (double) end.tv_sec * 1000000 + (double) end.tv_usec;
-
-//     printf("Your computation has used: %lf secs \n", (end_usec - start_usec)/(double)1000000);
-
-
-//     return (EXIT_SUCCESS);
-// }
-
-
-
-// Test main for TODO#3
-int main(int argc, char* argv[]){
-
-    //Check and parse command line options to be in the right format
-    if (argc < 2) {
-        printf("Usage: sum <infile> <numprocs>\n");
-        exit(EXIT_FAILURE);
-    }
-
-    //Limit number_of_processes into 10. 
-    //If there's no third argument, set the default number_of_processes into 1.  
-    if (argc < 3){
-        number_of_processes = 1;
-    }
-    else{
-        if (atoi(argv[2]) < MAX_PROCESS) number_of_processes = atoi(argv[2]);
-        else number_of_processes = MAX_PROCESS;
-    }
-
-    printf("Number of processes: %d\n", number_of_processes);
-    printf("Main process pid %d \n", getpid());
-
-    setup();
-    createchildren();
-
-    //enter exactly N jobs to the buffer
-    for (int i = 0; i<number_of_processes; i++){
-        shmPTR_jobs_buffer[i].task_type = 't';
-        shmPTR_jobs_buffer[i].task_duration = 1;
-        shmPTR_jobs_buffer[i].task_status = 1; //new, undone job
-        sem_post(sem_jobs_buffer[i]); // signal the child
-    }
-
-
-    //sleep for 3 seconds, the children processes should all finish by now
-    sleep(3);
-
-    //enter exactly N termination jobs to the buffer
-    for (int i = 0; i<number_of_processes; i++){
-        shmPTR_jobs_buffer[i].task_type = 'z';//termination job
-        shmPTR_jobs_buffer[i].task_duration = 1;
-        shmPTR_jobs_buffer[i].task_status = 1; 
-        sem_post(sem_jobs_buffer[i]); // signal the child
-    }
-
-    //wait for all N children processes
-    int waitpid_result;
-    for (int i = 0; i<number_of_processes; i++){
-        waitpid_result = waitpid(children_processes[i], NULL, 0); // returns when child exits normally
-        if (waitpid_result != -1){
-            printf("Child %d with pid %d has exited successfully\n", i, waitpid_result);
-        }
-    }
-
-        // print final results
-    printf("Final results: sum -- %ld, odd -- %ld, min -- %ld, max -- %ld, total task -- %ld\n", ShmPTR_global_data->sum_work, ShmPTR_global_data->odd, ShmPTR_global_data->min, ShmPTR_global_data->max, ShmPTR_global_data->total_tasks);
 
     //detach and remove shared memory locations
     int detach_status = shmdt((void *) ShmPTR_global_data); //detach
@@ -401,9 +294,51 @@ int main(int argc, char* argv[]){
         }
         free(sem_name);
     }
+}
+
+// Real main
+int main(int argc, char* argv[]){
+
+    printf("Lab 1 Starts...\n");
+
+    struct timeval start, end;
+    long secs_used,micros_used;
+
+    //start timer
+    gettimeofday(&start, NULL);
+
+    //Check and parse command line options to be in the right format
+    if (argc < 2) {
+        printf("Usage: sum <infile> <numprocs>\n");
+        exit(EXIT_FAILURE);
+    }
 
 
-    printf("success\n");
-    
-    return 0;
+    //Limit number_of_processes into 10. 
+    //If there's no third argument, set the default number_of_processes into 1.  
+    if (argc < 3){
+        number_of_processes = 1;
+    }
+    else{
+        if (atoi(argv[2]) < MAX_PROCESS) number_of_processes = atoi(argv[2]);
+        else number_of_processes = MAX_PROCESS;
+    }
+
+    setup();
+    createchildren();
+    main_loop(argv[1]);
+
+    //parent cleanup
+    cleanup();
+
+    //stop timer
+    gettimeofday(&end, NULL);
+
+    double start_usec = (double) start.tv_sec * 1000000 + (double) start.tv_usec;
+    double end_usec =  (double) end.tv_sec * 1000000 + (double) end.tv_usec;
+
+    printf("Your computation has used: %lf secs \n", (end_usec - start_usec)/(double)1000000);
+
+
+    return (EXIT_SUCCESS);
 }
